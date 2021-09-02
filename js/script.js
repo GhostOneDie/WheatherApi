bd366eccf5416f61de020489f4598c75f4e58ffb
GhostOneDie <moovierodger@gmail.com>
2021-09-02 14:01:49 +0300

    Commit to main

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $.getJSON('current.city.list.json', function(data){
+        $('select').on('change', function(){
+            var out='';
+            for (var key in data) {
+            if (data[key].country==$('select option:selected').val()){
+            out +=`<p value="${data[key].id}">${data[key].name}</p>`;
+            }
+        }
+        $('#city').html(out);
+        $('#city p').on('click', function(){
+            $.get(
+                "http://api.openweathermap.org/data/2.5/weather",
+                {
+                    "id" : $(this).attr('value'),
+                    "appid" : "b60328b260bb3b619e4e25dc1ccb314c"
+                },
+                function (data) {
+                    let out = '';
+                    out +='Погода: <b>'+data.weather[0].main+'</b><br>';
+                    out +='<p style="text-align:center"><img scr="https://openweathermap.org/img/w/'+data.weather[0].icon+'.png"</p>';
+                    out +='Температура: <b>'+Math.round(data.main.temp-273)+'&#176; C</b><br>';
+                    out +='Влажность: <b>'+data.main.humidity+'%</b><br>';
+                    out +='Давление: <b>'+Math.round(data.main.pressure*0.00750063755419211*100)+'мм.рт.ст</b><br>';
+                    out +='Видимость: <b>'+(data.visibility/1000)+'км</b><br>';
+                    console.log(data.main);
+                    $('#weather').html(out);
+                         } 
+                     );
+                });
+            })
+        });
+    });
+            
+        
